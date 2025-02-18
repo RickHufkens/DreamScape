bc227516ac445aaead9612d9cb1235b88c45397d
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2025-02-18 11:59:16 +0100

    Frame Creation

--- DreamScape/DreamScape/MainWindow.xaml.cs
+++ DreamScape/DreamScape/MainWindow.xaml.cs
@@ -1,3 +1,6 @@
+using DreamScape.Data;
+using Microsoft.UI.Windowing;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,16 +25,30 @@ namespace DreamScape
     /// <summary>
     /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainWindow : Window
+	public sealed partial class MainWindow : Window
     {
+        private bool isLoggedIn = false;
+        private List<User> Users;
+        public static Frame mainFrame { get; private set; }
         public MainWindow()
         {
+            using (var db = new AppDbContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
+            mainFrame = MainFrame;
             this.InitializeComponent();
-        }
+            MainFrame.Navigate(typeof(Login));
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
+            // Maximize window by setting the size
+            var hWnd = WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+            appWindow.SetPresenter(AppWindowPresenterKind.FullScreen); // Fullscreen mode
+
+            // Optional: remove the title bar
+            appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
         }
     }
 }
