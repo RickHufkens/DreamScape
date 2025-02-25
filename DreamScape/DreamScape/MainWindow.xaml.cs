a04f7a7aa08905d1927ee12acd2de3a4eaeb1ddf
RickHufkens <117633209+RickHufkens@users.noreply.github.com>
2025-02-25 09:16:26 +0100

    fixed small issues

--- DreamScape/DreamScape/MainWindow.xaml.cs
+++ DreamScape/DreamScape/MainWindow.xaml.cs
@@ -17,35 +17,30 @@ using Windows.Foundation;
 using Windows.Foundation.Collections;
 using WinRT.Interop;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace DreamScape
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-	public sealed partial class MainWindow : Window
+    public sealed partial class MainWindow : Window
     {
-        private bool isLoggedIn = false;
-        private List<User> Users;
+        public static int LoggedInUserId { get; set; }
         public static Frame mainFrame { get; private set; }
+
         public MainWindow()
         {
+            this.InitializeComponent();  // This should be here
             using (var db = new AppDbContext())
             {
                 db.Database.EnsureDeleted();
                 db.Database.EnsureCreated();
             }
+
             mainFrame = MainFrame;
-            this.InitializeComponent();
-            MainFrame.Navigate(typeof(Login));
+            MainFrame.Navigate(typeof(LoginPage));
 
             // Maximize window by setting the size
             var hWnd = WindowNative.GetWindowHandle(this);
             var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
             var appWindow = AppWindow.GetFromWindowId(windowId);
-            appWindow.SetPresenter(AppWindowPresenterKind.FullScreen); // Fullscreen mode
+            appWindow.SetPresenter(AppWindowPresenterKind.FullScreen);  // Fullscreen mode
 
             // Optional: remove the title bar
             appWindow.TitleBar.ExtendsContentIntoTitleBar = true;
